96700697cbfd0df2bfbfe94d568000c07affea5f
2480446748@qq.com <2480446748@qq.com>
2017-03-07 14:42:50 +0800

    v_test

---
+++ ezSmartHome/WebService/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebService.Controllers
+{
+    public class HomeController : Controller
+    {
+        static FileLog runLog = new FileLog(AppDomain.CurrentDomain.BaseDirectory + @"/log/runLog.txt");
+        [HttpPost]
+        public JsonResult Post( )
+        {
+
+            string json = Request.InputStream.ToString();
+            runLog.log(json);
+            var obj  = Newtonsoft.Json.JsonConvert.DeserializeObject<DingDongRequest>(json);
+            DingDongResponse r = new DingDongResponse();
+            return new JsonResult { Data = r  };
+        }
+    }
+}
