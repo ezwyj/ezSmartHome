ffc0d8edcb49b6cd8f1444ee42c46b964e57ca98
2480446748@qq.com <2480446748@qq.com>
2017-03-07 17:51:42 +0800

    20170307

--- ezSmartHome/WebService/Controllers/HomeController.cs
+++ ezSmartHome/WebService/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,8 +15,23 @@ namespace WebService.Controllers
         public JsonResult Post( )
         {
 
-            string json = Request.InputStream.ToString();
-            runLog.log(json);
+             Request.InputStream.ToString();
+            string postStr = string.Empty;
+            Stream inputStream = Request.InputStream;
+            int contentLength = Request.ContentLength;
+            int offset = 0;
+            if (contentLength > 0)
+            {
+                byte[] buffer = new byte[contentLength];
+                for (int i = inputStream.Read(buffer, offset, contentLength - offset); i > 0; i = inputStream.Read(buffer, offset, contentLength - offset))
+                {
+                    offset += i;
+                }
+                UTF8Encoding encoding = new UTF8Encoding();
+                postStr = encoding.GetString(buffer);
+            }
+            
+            runLog.log(postStr);
             //var obj  = Newtonsoft.Json.JsonConvert.DeserializeObject<DingDongRequest>(json);
             //DingDongResponse r = new DingDongResponse();
             return new JsonResult { Data = "OK"  };
